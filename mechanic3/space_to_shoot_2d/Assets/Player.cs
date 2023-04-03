88936aabe1d43d25bdd5e4f8988419e75e903f3b
Joseph Collora <j.collora@outlook.com>
2023-04-02 20:03:05 -0700

    implemented mechanic3

--- mechanic3/space_to_shoot_2d/Assets/Player.cs
+++ mechanic3/space_to_shoot_2d/Assets/Player.cs
@@ -47,10 +47,18 @@ public class Player : MonoBehaviour {
     }
     
     private void ProcessBulletSpwan() {
-      if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Mouse0)) && Time.time > nextFire) {
+      if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)) && Time.time > nextFire) {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D re = bullet.GetComponent<Rigidbody2D>();
-        re.velocity = firePoint.up * bulletSpeed;
+        if (Input.GetKey(KeyCode.UpArrow)) {
+          re.velocity = Vector2.up * bulletSpeed;
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
+          re.velocity = Vector2.down * bulletSpeed;
+        } else if (Input.GetKey(KeyCode.LeftArrow)) {
+          re.velocity = Vector2.left * bulletSpeed;
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+          re.velocity = Vector2.right * bulletSpeed;
+        }
         nextFire = Time.time + cooldown;
       }
     }
